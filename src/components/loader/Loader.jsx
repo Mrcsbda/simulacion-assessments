5e464b093a521d914329b74e417f13b0918f65e0
mrcsbda <macbedoya@gmail.com>
2023-08-16 11:53:28 -0500

    login finished

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,8 +1,21 @@
+import { Spinner } from '@chakra-ui/react'
 import React from 'react'
+import "./loader.scss"
 
 const Loader = () => {
+  const white = "#ffffff"
+  const purple ="#B1B1B1"
+
   return (
-    <div>Loader</div>
+    <main className='spinner'>
+      <Spinner className='spinner__loader'
+        thickness='4px'
+        speed='0.65s'
+        emptyColor={purple}
+        color={white}
+        size='xl'
+      />
+    </main>
   )
 }
 
---
+++ src/components/projectTasks/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectTask = () => {
+  return (
+    <div>ProjectTask</div>
+  )
+}
+
+export default ProjectTask
