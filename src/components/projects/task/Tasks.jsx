48e5b06c759b244f63ecb7061e5b16b10fa7aa7c
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-16 13:21:52 -0500

    prototype

---
+++ src/components/projects/task/Tasks.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+
+const Tasks = ({tasks}) => {
+  return (
+
+      <ul className="Layout__projects__tasks">
+        {tasks.map((task)=>(
+            <li key={task.id} className="Layout__projects__task">
+              <input type="checkbox" checked={task.completed} />
+              <span>{task.title}</span>  
+              
+            </li>
+        ))}
+      </ul>
+  )
+}
+
+export default Tasks
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateProjects } from "../../store/slices/projectSlice/projectSlice"
+import Tasks from '../../components/projects/task/Tasks'
+import "./Layout.scss"
+
 
 const Layout = () => {
 
@@ -13,12 +16,18 @@ const Layout = () => {
   return (
     <>
       <main className="Layout__container">
-        <section className="Layout__container__projects">
+<section> 
+</section>
+
+        <section className="Layout__container__manager">
+          <h1 className="Layout__title" >{`Gestor de proyectos de ${userInfo.name}`}</h1>
           {
-            <ul>
+            <ul className="Layout__projects">
               {projects.map((project) => (
-                <li key={project.id}>
-                  {project.title}
+                <li key={project.id} className="Layout__projects__project">
+                 <h2>{project.title}</h2> 
+
+                  <Tasks tasks={project.tasks}/>
                 </li>
               ))}
             </ul>
